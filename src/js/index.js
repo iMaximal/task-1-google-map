59a34b61beb94512340c721d7933c4e2d34b3878
Maximal <maxim@infofree.ru>
2017-09-22 00:57:59 +0500

    -

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import Main from './containers/Main'
-import { store, history } from './store'
+import { store } from './store'
 
 
 render(
