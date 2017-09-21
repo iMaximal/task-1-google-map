769a35af8d111c560c5966e0426561963a1ffd07
Maximal <maxim@infofree.ru>
2017-09-21 21:50:44 +0500

    Refactor 3/4

--- src/js/components/NoteItem.js
+++ src/js/components/NoteItem.js
@@ -8,83 +8,66 @@ import {
 } from '../actions'
 
 @connect(({map, markers}) => ({map, markers}))
-export default class NoteItem extends React.Component { // todo
+export default class NoteItem extends PureComponent {
 
     /**
-     * If click Edit note -> get Note name from Global store for edit form -> Save & mark input in Local state
+     * If click Edit note -> Mark Note in Global store
+     * @param key - noteId
      * @param event
      */
-    NoteEditHandler = (key, value, event) => {
+    NoteEditHandler = (key, event) => {
         event.preventDefault()
         // if editable field is exist -> nothing
-        if (this.props.map.editablePoint || this.props.map.editableNote) return
+        if (this.props.map.editablePoint || this.props.map.editableNote) return false
 
-        this.props.dispatch(changeMapStore({ //todo change name in all files
+        this.props.dispatch(changeMapStore({
             editableNote: key
         }))
-
     }
 
     /**
-     * Cancel edit -> Change local params to null -> Data from Global store (nothing change)
+     * Cancel edit -> Change Global params to null
      * @param event
      */
     NoteCancelHandler = (event) => {
         event.preventDefault()
 
-        this.props.dispatch(changeMapStore({ //todo change name in all files
+        this.props.dispatch(changeMapStore({
             newNote: false,
             editableNote: null
         }))
-
-        this.setState({
-            noteNameEdit: ''
-        })
-
     }
 
-    /**
-     * Write Note data from Local state to Global store
-     * @param event
-     */
-    NoteSaveHandler = (event) => {
+    NoteSaveHandler = (parentId, noteId, event) => {
         event.preventDefault()
 
-        const noteNameEdit = ReactDOM.findDOMNode(this.refs.noteEditable).value.trim() || 'Нельзя менять на пустое имя'
-        const noteKey = event.target.closest('li').dataset.nkey
-        const parentId = event.target.closest('li').dataset.id
+        const noteName = ReactDOM.findDOMNode(this.refs.noteEditable).value.trim() || 'Нельзя менять на пустое имя'
 
-        this.props.dispatch(changeNote(parentId, noteNameEdit, noteKey))
+        this.props.dispatch(changeNote(parentId, noteName, noteId))
 
-        this.props.dispatch(changeMapStore({ //todo change name in all files
+        this.props.dispatch(changeMapStore({
             newNote: false,
             editableNote: null
         }))
-
-        this.setState({
-            noteNameEdit: ''
-        })
     }
 
     /**
      * Delete Note from Global store
      * @param event
      */
-    NoteRemoveHandler = (event) => {
+    NoteRemoveHandler = (parentId, noteId, event) => {
         event.preventDefault()
 
         // if editable field is exist -> nothing
         if (this.props.map.editablePoint || this.props.map.editableNote) return
 
-        const parentId = event.target.closest('li').dataset.id
-        const noteId = event.target.closest('li').dataset.nkey
-
         this.props.dispatch(deleteNote(parentId, noteId))
-
     }
 
 
     render() {
+
+        const {parentId} = this.props
 
         return (
 
@@ -92,7 +75,7 @@ export default class NoteItem extends React.Component { // todo
             <ul>
                 {this.props.notes.map(note => Object.entries(note).map(([key, value]) => (
                     <li key={key}
-                        data-id={this.props.parentId}
+                        data-id={parentId}
                         data-nkey={key}
                         className="right-side__point">
 
@@ -101,7 +84,7 @@ export default class NoteItem extends React.Component { // todo
                          */}
                         {this.props.map.editableNote === key &&
                         <div className="edit-li">
-                            <form onSubmit={this.NoteSaveHandler}>
+                            <form onSubmit={this.NoteSaveHandler.bind(this, parentId, key)}>
                                 <textarea autoFocus
                                           className="edit-area"
                                           defaultValue={value}
@@ -123,18 +106,17 @@ export default class NoteItem extends React.Component { // todo
                         }
 
                         <div className="controls-container">
-                            <a onClick={this.NoteEditHandler.bind(this, key, value)} href=""
+                            <a onClick={this.NoteEditHandler.bind(this, key)} href=""
                                className="controls"
-                               title="Edit Notes">Edit</a> &nbsp;
-                            <a onClick={this.NoteRemoveHandler}
+                               title="Edit Note">Edit</a> &nbsp;
+                            <a onClick={this.NoteRemoveHandler.bind(this, parentId, key)}
                                href=""
                                className="controls"
-                               title="Remove Notes">Remove</a>
+                               title="Remove Note">Remove</a>
                         </div>
                     </li>
                 )))}
             </ul>
         )
     }
-
 }
